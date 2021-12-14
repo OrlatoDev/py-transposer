469d190681ed13bd8965167b7a8591c99c39176a
Samuel <samuelorlato@gmail.com>
2021-12-14 01:22:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+allNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+relatives = {
+    "C": "Am",
+    "G": "Em",
+    "D": "Bm",
+    "A": "F#m",
+    "E": "C#m",
+    "B": "G#m",
+    "F#": "D#m",
+    "C#": "A#m",
+    "F": "Dm",
+    "A#": "Gm",
+    "D#": "Cm",
+    "G#": "Fm"
+}
+
+notes = str(input("Notes: "))
+fromTone = str(input("From tone: "))
+toTone = str(input("To tone: "))
+
+notes = notes.split(", ")
+
+if "m" not in fromTone and "m" not in toTone or "m" in fromTone and "m" in toTone:
+    iFromTone = allNotes.index(fromTone)
+    iToTone = allNotes.index(toTone)
+
+    semitones = iToTone - iFromTone
+
+    notesTransposed = []
+
+    for note in notes:
+        iNote = allNotes.index(note)
+        IToNote = iNote + semitones
+
+        if IToNote > (len(allNotes) - 1):
+            IToNote = IToNote - len(allNotes)
+
+        transposedNote = allNotes[IToNote]
+        notesTransposed.append(transposedNote)
+
+    print(notesTransposed)
+
+else:
+    if "m" in fromTone:
+        toTone = relatives[toTone]
+    
+    if "m" in toTone:
+        fromTone = relatives[fromTone]
+
+    iFromTone = allNotes.index(fromTone[:fromTone.find("m")])
+    iToTone = allNotes.index(toTone[:fromTone.find("m")])
+
+    semitones = iToTone - iFromTone
+
+    notesTransposed = []
+
+    for note in notes:
+        iNote = allNotes.index(note)
+        IToNote = iNote + semitones
+
+        if IToNote > (len(allNotes) - 1):
+            IToNote = IToNote - len(allNotes)
+
+        transposedNote = allNotes[IToNote]
+        notesTransposed.append(transposedNote)
+
+    print(notesTransposed)
